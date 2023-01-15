080ea139e76084d4250f8b26d4945eb9963387fb
Alysia <alysia@bench.co>
2023-01-15 10:51:26 -0800

    add expo quickstart boilerplate

---
+++ App.js
@@ -0,0 +1,25 @@
+import React from "react";
+import {
+  SafeAreaView,
+  Text,
+  StyleSheet,
+  Platform,
+  StatusBar,
+} from "react-native";
+
+const App = () => {
+  return (
+    <SafeAreaView style={styles.container}>
+      <Text>Hello World</Text>
+    </SafeAreaView>
+  );
+};
+
+export default App;
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    paddingTop: Platform.OS === "android" ? StatusBar.currentHeight : 0,
+  },
+});
